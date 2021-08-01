1ed21bde957f8b4dc573e4767b4ab54d05019e2c
Yankang <yankang.zhu@icloud.com>
2021-08-01 21:06:00 +0100

    Add login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'instagram-data-viewer';
   navLinks = [
-    { path: "/posts", label: "Posts" },
-    { path: "/processed-posts", label: "Processed Posts" },
-    { path: "/upload", label: "Uploads" }
-  ]
+    { path: '/posts', label: 'Posts' },
+    { path: '/processed-posts', label: 'Processed Posts' },
+    { path: '/upload', label: 'Uploads' },
+  ];
+
+  constructor(public auth: AngularFireAuth, private router: Router) {}
+
+  login() {
+    this.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(() => this.router.navigate(['/']));
+  }
+
+  logout() {
+    this.auth.signOut().then(() => this.router.navigate(['/']));
+  }
 }
