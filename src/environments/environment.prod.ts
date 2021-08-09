36427747b8652e207d390cacad407633f59a32f1
Yankang <yankang.zhu@icloud.com>
2021-08-09 16:44:26 +0100

    Hide API keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,18 @@
+/* Firebase config are set s the following
+ * firebase functions:config:set prod.apikey=XXXX
+ *
+ * To check where set is successful
+ * firebase functions:config:get
+ */
 export const environment = {
   production: true,
-  firebase:
-  {
-    apiKey: ***REMOVED***,
-    authDomain: "instagram-data-label-tool.firebaseapp.com",
-    projectId: "instagram-data-label-tool",
-    storageBucket: "instagram-data-label-tool.appspot.com",
-    messagingSenderId: "531391085958",
-    appId: "1:531391085958:web:367bd4a9ce646c92db0007",
-    measurementId: "G-P4R3NJ4YC1"
-  }
+  firebase: {
+    apiKey: 'ADD YOUR API KEY',
+    authDomain: 'instagram-data-label-tool.firebaseapp.com',
+    projectId: 'instagram-data-label-tool',
+    storageBucket: 'instagram-data-label-tool.appspot.com',
+    messagingSenderId: '531391085958',
+    appId: '1:531391085958:web:367bd4a9ce646c92db0007',
+    measurementId: 'G-P4R3NJ4YC1',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { environment as prod_environment } from './environment.prod';
 
-export const environment = {
-  production: false,
-  firebase:
-  {
-    apiKey: ***REMOVED***,
-    authDomain: "instagram-data-label-tool.firebaseapp.com",
-    projectId: "instagram-data-label-tool",
-    storageBucket: "instagram-data-label-tool.appspot.com",
-    messagingSenderId: "531391085958",
-    appId: "1:531391085958:web:367bd4a9ce646c92db0007",
-    measurementId: "G-P4R3NJ4YC1"
-  }
-
-};
+// Use production environement for now
+export const environment = prod_environment;
 
 /*
  * For easier debugging in development mode, you can import the following file
